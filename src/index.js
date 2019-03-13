77435fe9ef53ac445457ae2cff84423634f33dd3
jzc <justincai.lps@gmail.com>
2019-03-13 17:47:56 -0600

    added rotation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import * as THREE from "three";
 let container;
 let camera, scene, renderer;
 let uniforms;
+let pivot1, rod1, pivot2, rod2, pivot3;
+const rh = .25;
+const pz = 0;
+const rz = -.5;
+const axis = new THREE.Vector3(0, 0, 1);
 
 const vs = `
     varying vec2 vUv;
@@ -32,37 +37,50 @@ const fs = `
 
 init();
 animate();
+setAngle(2.7*Math.PI, 0.5*Math.PI);
 
 function init() {
     container = document.getElementById('container');
     scene = new THREE.Scene();
     camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-    let geometry = new THREE.PlaneBufferGeometry(2, 2);
-    uniforms = {
-        "uTime": { value: 1.0 }
-    };
-    let material = new THREE.ShaderMaterial({
-        uniforms: uniforms,
-        vertexShader: vs,
-        fragmentShader: fs
-    });
-    let mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    let pivotMaterial = new THREE.MeshBasicMaterial({ color: "0x2194ce"})
-    let pivot1 = new THREE.CircleBufferGeometry(0.01, 32);
-    let pivotMesh = new THREE.Mesh(pivot1, pivotMaterial);
-    // pivotMesh.matrix.makeTranslation(-1, -1, 0);
-    // pivotMesh.matrixAutoUpdate = false;
-    scene.add(pivotMesh);
-
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     container.appendChild(renderer.domElement);
 
-
     onWindowResize();
     window.addEventListener('resize', onWindowResize, false);
+
+    uniforms = { "uTime": { value: 0.0 } };
+    let noiseMesh = new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(2, 2),
+        new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: vs,
+            fragmentShader: fs
+        }),
+    );
+    scene.add(noiseMesh);
+
+    pivot1 = createMeshObj(new THREE.CircleBufferGeometry(0.025, 64), new THREE.MeshBasicMaterial);
+    pivot1.mat.color.set("#3177ea");
+    
+    rod1 = createMeshObj(new THREE.PlaneBufferGeometry(0.025, rh), new THREE.MeshBasicMaterial);
+    rod1.mat.color.set("#4286f4");
+    
+    pivot2 = createMeshObj(new THREE.CircleBufferGeometry(0.025, 64), new THREE.MeshBasicMaterial);
+    pivot2.mat.color.set("#3177ea");
+    
+    rod2 = createMeshObj(new THREE.PlaneBufferGeometry(0.025, rh), new THREE.MeshBasicMaterial);
+    rod2.mat.color.set("#4286f4");
+    
+    pivot3 = createMeshObj(new THREE.CircleBufferGeometry(0.025, 64), new THREE.MeshBasicMaterial);
+    pivot3.mat.color.set("#3177ea"); 
+}
+
+function createMeshObj(geometry, mat) {
+    let mesh = new THREE.Mesh(geometry, mat);
+    scene.add(mesh);
+    return {mesh: mesh, mat: mat, geometry: geometry};
 }
 
 function onWindowResize() {
@@ -76,4 +94,30 @@ function animate(timestamp) {
     requestAnimationFrame(animate);
     uniforms["uTime"].value = timestamp / 1000;
     renderer.render(scene, camera);
+    // console.log(timestamp);
+}
+
+function setAngle(th1, th2) {
+    pivot1.mesh.position.set(0, 0, pz);
+
+    rod1.mesh.position.set(0, -rh/2, rz);
+    rod1.mesh.position.applyAxisAngle(axis, th1);
+    rod1.mesh.quaternion.setFromAxisAngle(axis, th1);
+    
+    pivot2.mesh.position.set(0, -rh, pz);
+    pivot2.mesh.position.applyAxisAngle(axis, th1);
+
+    rod2.mesh.position.set(0, -3*rh/2, rz);
+    rod2.mesh.position.applyAxisAngle(axis, th1);
+    rod2.mesh.position.sub(pivot2.mesh.position);
+    rod2.mesh.position.applyAxisAngle(axis, th2);
+    rod2.mesh.position.add(pivot2.mesh.position);
+
+    rod2.mesh.quaternion.setFromAxisAngle(axis, th1+th2);
+
+    pivot3.mesh.position.set(0, -2*rh, rz);
+    pivot3.mesh.position.applyAxisAngle(axis, th1);
+    pivot3.mesh.position.sub(pivot2.mesh.position);
+    pivot3.mesh.position.applyAxisAngle(axis, th2);
+    pivot3.mesh.position.add(pivot2.mesh.position);
 }
