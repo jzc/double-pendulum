c42d6ce7f057701a612a236ca9f24e175824ba61
jzc <justincai.lps@gmail.com>
2019-03-13 18:34:19 -0600

    rk4 and pendulum eqn

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ let container;
 let camera, scene, renderer;
 let uniforms;
 let pivot1, rod1, pivot2, rod2, pivot3;
-const rh = .25;
+const rh = .3;
 const pz = 0;
 const rz = -.5;
 const axis = new THREE.Vector3(0, 0, 1);
+const g = 9.8;
 
 const vs = `
     varying vec2 vUv;
@@ -35,9 +36,23 @@ const fs = `
     }
 `;
 
+let th0 = .25*Math.PI, th1 = 0, pth0 = 0, pth1 = 0;
+let m = .01, l = .2;
+let f = makeDoublePendulumEqn(m, l);
+let h = .005;
+
 init();
 animate();
-setAngle(2.7*Math.PI, 0.5*Math.PI);
+
+function animate(timestamp) {
+    requestAnimationFrame(animate);
+    uniforms["uTime"].value = timestamp / 1000;
+    
+    setAngle(th0, th1);
+    renderer.render(scene, camera);
+    
+    [th0, th1, pth0, pth1] = rk4Step(f, [th0, th1, pth0, pth1], h);
+}
 
 function init() {
     container = document.getElementById('container');
@@ -90,13 +105,6 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function animate(timestamp) {
-    requestAnimationFrame(animate);
-    uniforms["uTime"].value = timestamp / 1000;
-    renderer.render(scene, camera);
-    // console.log(timestamp);
-}
-
 function setAngle(th1, th2) {
     pivot1.mesh.position.set(0, 0, pz);
 
@@ -120,4 +128,38 @@ function setAngle(th1, th2) {
     pivot3.mesh.position.sub(pivot2.mesh.position);
     pivot3.mesh.position.applyAxisAngle(axis, th2);
     pivot3.mesh.position.add(pivot2.mesh.position);
+}
+
+function vectorScale(v, c) {
+    return v.map(x => x*c);
+}
+
+function vectorAdd(...vs) {
+    return vs.reduce(function (acc, v) {
+        return acc.map((x, i) => x+v[i])
+    }, Array(vs[0].length).fill(0));
+}
+
+function rk4Step(f, yn, h) {
+    let k1 = vectorScale(f(yn), h);
+    let k2 = vectorScale(f(vectorAdd(yn, vectorScale(k1, 1/2))), h);
+    let k3 = vectorScale(f(vectorAdd(yn, vectorScale(k2, 1/2))), h);
+    let k4 = vectorScale(f(vectorAdd(yn, k3)), h);
+    return vectorAdd(yn, vectorScale(vectorAdd(
+        k1,
+        vectorScale(k2, 2),
+        vectorScale(k3, 2),
+        k4,
+    ), 1/6));
+}
+
+function makeDoublePendulumEqn(m, l) {
+    return function(x) {
+        let [th1, th2, pth1, pth2] = x;
+        let th1d = (6/(m*l**2)) * ((2*pth1-3*Math.cos(th1-th2)*pth2)/(16-9*(Math.cos(th1-th2)**2)));
+        let th2d = (6/(m*l**2)) * ((8*pth2-3*Math.cos(th1-th2)*pth1)/(16-9*(Math.cos(th1-th2)**2)));
+        let pth1d = ((-1/2)*m*(l**2)) * (th1d*th2d*Math.sin(th1-th2)+3*g/l*Math.sin(th1));
+        let pth2d = ((-1/2)*m*(l**2)) * (-th1d*th2d*Math.sin(th1-th2)+g/l*Math.sin(th2));
+        return [th1d, th2d, pth1d, pth2d];
+    }
 }
