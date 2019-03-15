8bf583b198670e2b01781c58c384dd15a48bc693
jzc <justincai.lps@gmail.com>
2019-03-14 21:37:50 -0600

    made it really cool

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
+import { MeshLine, MeshLineMaterial } from "three.meshline";
 
 let container;
 let camera, scene, renderer;
 let uniforms;
 // let pivot1, rod1, pivot2, rod2, pivot3;
-const rh = .3;
+const rh = .5;
 const pz = 0;
 const rz = -.5;
 const axis = new THREE.Vector3(0, 0, 1);
@@ -27,7 +28,7 @@ const fs = `
     float random (vec2 st) {
         return fract(
             sin(
-                dot(st.xy, vec2(12.9898,78.233))//+uTime*.2
+                dot(st.xy, vec2(12.9898,78.233))+uTime*.2
             )*43758.5453123
         );
     }
@@ -55,13 +56,15 @@ const fs = `
 
     void main()	{
         // vec2 uv = vUv;
-        // gl_FragColor = vec4(vec3(noise(10.0*uv)), 1);
-        gl_FragColor = vec4(1, 1, 1, 1);
+        // gl_FragColor = vec4(vec3(noise(64.0*uv)), 1.0);
+        gl_FragColor = vec4(0, 0, 0, 1);
     }
 `;
 
+const trajectory_length = 150;
+
 class Pendulum {
-    constructor(rodColor, pivotColor, m, l, th0, th1) {
+    constructor(rodColor, pivotColor, trajectoryColor, m, l, th1, th2) {
         this.pivot1 = createMeshObj(new THREE.CircleBufferGeometry(0.025, 64), new THREE.MeshBasicMaterial);
         this.pivot1.mat.color.set(pivotColor);
         
@@ -77,47 +80,71 @@ class Pendulum {
         this.pivot3 = createMeshObj(new THREE.CircleBufferGeometry(0.025, 64), new THREE.MeshBasicMaterial);
         this.pivot3.mat.color.set(pivotColor); 
 
-        this.reset(th0, th1);
+        this.reset(th1, th2);
         this.f = makeDoublePendulumEqn(m, l);
+        this.trajectory_geometry = new THREE.Geometry();
+        for (let i = 0; i < trajectory_length; i++) {
+            this.trajectory_geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+        }
+        this.trajectory_line = new MeshLine();
+        this.trajectory_line.setGeometry(this.trajectory_geometry, p => p);
+        this.trajectory_material = new MeshLineMaterial({
+            color: trajectoryColor, 
+            lineWidth: .005,
+            transparent: true,
+            opacity: .5,
+        })
+        // this.trajectory_material.transparent = true;
+        // this.trajectory_material.opacity = .5;
+
+        this.trajectory_mesh = new THREE.Mesh(this.trajectory_line.geometry, this.trajectory_material);
+        scene.add(this.trajectory_mesh);
     }
 
-    reset(th0, th1) {
-        this.th0 = th0;
+    // updateTrajectory() {
+    //     this.line.verticesNeedUpdate = true;
+    //     this.geometry.verticesNeedUpdate = true;
+    //     if (this.geometry.vertices.length >= trajectory_length) {
+    //         this.geometry.vertices.shift();
+    //     } 
+    //     this.geometry.vertices.push(new THREE.Vector3(this.th1, this.th2, 0));
+    //     this.line.setGeometry(this.geometry);
+    // }
+
+    reset(th1, th2) {
         this.th1 = th1;
-        this.pth0 = 0;
+        this.th2 = th2;
         this.pth1 = 0;
+        this.pth2 = 0;
     }
 
     // Unlike th0 and th1 from the pendulum equation, th1 is the angle of the 
     // lower rod as measured from th0.
-    setAngle(th0, th1) {
+    setAngle(th1, th2) {
         this.pivot1.mesh.position.set(0, 0, pz);
-
-        this.rod1.mesh.position.set(0, -rh/2, rz);
-        this.rod1.mesh.position.applyAxisAngle(axis, th0);
-        this.rod1.mesh.quaternion.setFromAxisAngle(axis, th0);
         
-        this.pivot2.mesh.position.set(0, -rh, pz);
-        this.pivot2.mesh.position.applyAxisAngle(axis, th0);
+        let sinTh1 = Math.sin(th1);
+        let cosTh1 = Math.cos(th1);
+        let sinTh2 = Math.sin(th2);
+        let cosTh2 = Math.cos(th2);
+        
 
-        this.rod2.mesh.position.set(0, -3*rh/2, rz);
-        this.rod2.mesh.position.applyAxisAngle(axis, th0);
-        this.rod2.mesh.position.sub(this.pivot2.mesh.position);
-        this.rod2.mesh.position.applyAxisAngle(axis, th1);
-        this.rod2.mesh.position.add(this.pivot2.mesh.position);
+        this.rod1.mesh.position.set(rh/2*sinTh1, -rh/2*cosTh1, rz);
+        this.rod1.mesh.quaternion.setFromAxisAngle(axis, th1);
+        
+        this.pivot2.mesh.position.set(rh*sinTh1, -rh*cosTh1, pz);
 
-        this.rod2.mesh.quaternion.setFromAxisAngle(axis, th0+th1);
+        this.rod2.mesh.position.set(rh*(sinTh1 + sinTh2/2), -rh*(cosTh1 + cosTh2/2), rz);
+        this.rod2.mesh.quaternion.setFromAxisAngle(axis, th2);
 
-        this.pivot3.mesh.position.set(0, -2*rh, rz);
-        this.pivot3.mesh.position.applyAxisAngle(axis, th0);
-        this.pivot3.mesh.position.sub(this.pivot2.mesh.position);
-        this.pivot3.mesh.position.applyAxisAngle(axis, th1);
-        this.pivot3.mesh.position.add(this.pivot2.mesh.position);
+        this.pivot3.mesh.position.set(rh*(sinTh1 + sinTh2), -rh*(cosTh1 + cosTh2), rz);
     }
 
     step(h) {
-        [this.th0, this.th1, this.pth0, this.pth1] = rk4Step(this.f, [this.th0, this.th1, this.pth0, this.pth1], h);
-        this.setAngle(this.th0, this.th1-this.th0);
+        [this.th1, this.th2, this.pth1, this.pth2] = rk4Step(this.f, [this.th1, this.th2, this.pth1, this.pth2], h);
+        this.setAngle(this.th1, this.th2);
+        this.trajectory_line.advance(this.pivot3.mesh.position);
+        // this.updateTrajectory();    
     }
 }
 
@@ -152,7 +179,6 @@ function init() {
 
     let noiseMesh = new THREE.Mesh(
         new THREE.PlaneBufferGeometry(2, 2),
-        // new THREE.MeshBasicMaterial,
         new THREE.ShaderMaterial({
             uniforms: uniforms,
             vertexShader: vs,
@@ -161,15 +187,17 @@ function init() {
     );
     scene.add(noiseMesh);
 
-    for (let i = 0; i < 20; i++) {
-        let th = 0.9*Math.PI + .00001*i;
-        pendulums.push(new Pendulum("#3177ea", "#4286f4", .1, .1, th, th));
+    let n = 20
+    let r1 = Math.PI*Math.random()+Math.PI/2, r2 = 2*Math.PI*Math.random();
+    for (let i = 0; i < n; i++) {
+        let th = r1 + .00001*i;
+        pendulums.push(new Pendulum(
+            new THREE.Color().setHSL(i/(n-1), .45, .7),
+            new THREE.Color().setHSL(i/(n-1), .45, .6),
+            new THREE.Color().setHSL(i/(n-1), .9, .6),
+             .1, .1, th, th + r2)
+        );
     }
-
-    // pendulums.push(new Pendulum("#3177ea", "#4286f4", .1, .1, 0.500*Math.PI, 0.501*Math.PI));
-    // pendulums.push(new Pendulum("#3177ea", "#4286f4", .1, .1, 0.501*Math.PI, 0.502*Math.PI));
-    // pendulums.push(new Pendulum("#3177ea", "#4286f4", .1, .1, 0.502*Math.PI, 0.503*Math.PI));
-    // pendulums.push(new Pendulum("#3177ea", "#4286f4", .1, .1, 0.503*Math.PI, 0.504*Math.PI));
 }
 
 function createMeshObj(geometry, mat) {
@@ -213,12 +241,18 @@ function rk4Step(f, yn, h) {
 // source: https://en.wikipedia.org/wiki/Double_pendulum
 // Note: th1 and th2 are angle of the the respective rod as measured from the vertical.
 function makeDoublePendulumEqn(m, l) {
+    let d = (m*l**2);
+    let a = (6/d);
+    let f = g/l;
     return function(x) {
         let [th1, th2, pth1, pth2] = x;
-        let th1d = (6/(m*l**2)) * ((2*pth1-3*Math.cos(th1-th2)*pth2)/(16-9*(Math.cos(th1-th2)**2)));
-        let th2d = (6/(m*l**2)) * ((8*pth2-3*Math.cos(th1-th2)*pth1)/(16-9*(Math.cos(th1-th2)**2)));
-        let pth1d = ((-1/2)*m*(l**2)) * (th1d*th2d*Math.sin(th1-th2)+3*g/l*Math.sin(th1));
-        let pth2d = ((-1/2)*m*(l**2)) * (-th1d*th2d*Math.sin(th1-th2)+g/l*Math.sin(th2));
+        let b = 3*Math.cos(th1-th2);
+        let c =  (16-9*(Math.cos(th1-th2)**2));
+        let th1d = a * (2*pth1-b*pth2)/c;
+        let th2d = a * (8*pth2-b*pth1)/c;
+        let e = th1d*th2d*Math.sin(th1-th2);
+        let pth1d = (-d/2) * (e+3*f*Math.sin(th1));
+        let pth2d = (-d/2) * (-e+f*Math.sin(th2));
         return [th1d, th2d, pth1d, pth2d];
     }
 }
